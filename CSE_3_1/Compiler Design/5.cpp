7459ccfd1d81b8fb4b77e84af42245c49024fc19
ApurbaCD <apurbadey31273127@gmail.com>
2024-06-02 00:06:25 +0600

    add in 02/06/2024

--- CSE_3_1/Compiler Design/5.cpp
+++ CSE_3_1/Compiler Design/5.cpp
@@ -23,12 +23,103 @@ void ans(){
     }
     string line;
     while(getline(in,line)){
-        cout<<line<<endl;
-        if(line[0]>=65&&line[0]<=90)&&||(line[0])
+        bool b=1;
+        int cnt = 0;
+      //  cout<<line<<endl;
+        for ( int i = 0; i<line.size(); i++ ){
+            if(line[i]=='.')cnt++;
+        }
+        if((line[0]>='A'&&line[0]<=72)||(line[0]>=97&&line[0]<=104)||(line[0]>=111&&line[0]<=122)||(line[0]>=89&&line[0]<=90)){
+            for(int i=1;i<line.size();i++){
+                if((line[i]>=65&&line[i]<=90)||(line[i]>=97&&line[i]<=122)||(line[i]>=48&&line[i]<=97)){
+                    continue;
+                }
+                else{
+                    cout<<"Otherwise\n";
+                    b=0;break;
+                }
+            }
+            if(b){
+                cout<<"Float variable\n";
+            }
 
+        }
+        else if(cnt==1){
+            bool b1=1;
+            if(line[0]=='0'){
+                if(line[1]=='.'){
+                    int digit_cnt=0;
+                    for(int i=2;i<line.size();i++){
+                        if(line[i]>='0'&&line[i]<='9'){
+                            digit_cnt++;
+                        }
+                        else{
+                            b1=0;
+                            cout<<"Otherwise\n";
+                            break;
+                        }
+                    }
+                    if(b1){
+                    if(digit_cnt==2){
+                        cout<<"Float\n";
+                    }   
+                    else if(digit_cnt>=3){
+                        cout<<"double\n";
+                    }
+                    else{
+                        cout<<"Otherwise\n";
+                    }
+                    }
+                }
+            }
+
+            else{
+                bool b1=1,b2=1;
+                if(line[0]>='1' && line[0]<='9'){
+                    int i=0;
+                    while(line[i]!='.'){
+                        if(line[i]>='0' && line[i]<='9'){
+                            i++;continue;
+                        }
+                        else{
+                            cout<<"Otherwise\n";
+                            b1=0;break;
+                        }
+                    }
+                    if(b1){
+                        int digit_cnt1=0;
+                        for(int j=i;j<line.size();j++){
+                           if(line[j]>='0' && line[j]<='9'){
+                            digit_cnt1++;
+                            continue;
+                            }
+                            else if(line[j]=='.')continue;
+                            else{
+                                b2=0;
+                            }
+                        }
+                        if(b1&&b2){
+                            if(digit_cnt1==2){
+                                cout<<"Float\n";
+                            }   
+                            else if(digit_cnt1>=3){
+                                cout<<"double\n";
+                            }
+                            else{
+                                cout<<"Otherwise\n";
+                            }
+                        }
+                        
+
+                    }
+                }
+            }
+        }
+        else{
+            cout<<"Otherwise\n";
+        }
     }
 }
-
 
 int main(){
   ios::sync_with_stdio(false);cin.tie(0);
